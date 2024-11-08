a3abdf8e89cd6d5760be5a16fbcb6f69fc5be9e8
ghaidaahaj42 <gaidaahaj2@gmail.com>
2024-11-09 00:20:48 +0200

    start page done

--- src/components/GameLogic/StartPage.jsx
+++ src/components/GameLogic/StartPage.jsx
@@ -1,20 +1,60 @@
-import React from "react";
 import Slider from "react-slider";
 import { useState } from "react";
+import Typewriter from "typewriter-effect";
 export default function StartPage() {
   const [value, setValue] = useState(50);
-  const handleSliderChange = (val) => {
+  const [player1, setPlayer1] = useState("");
+  const [player2, setPlayer2] = useState("");
+  const [notification, showNotification] = useState("");
+  function handleSliderChange(val) {
     setValue(val);
-  };
+  }
+  function handleNameChange(playerNum, val) {
+    if (playerNum === 1) {
+      setPlayer1(val);
+    } else {
+      setPlayer2(val);
+    }
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!player1 || !player2) {
+      showNotification("ENTER PLAYER NAMES");
+    }
+  }
   return (
     <div className="home taller">
-      <form>
-        <h2>Enter Player Names</h2>
+      <form form onSubmit={handleSubmit}>
+        <Typewriter
+          options={{
+            strings: ["PLS, ENTER PLAYER NAMES"],
+            autoStart: true,
+            loop: false,
+
+            deleteSpeed: 1000000000000000,
+            backSpeed: 1000000000000000,
+          }}
+        />
+        {notification && <p className="notification">MUST ENTER NAMES</p>}
         <label>
-          <input type="text" name="name" placeholder="Player 1 Name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Player 1 Name"
+            onChange={(event) => {
+              handleNameChange(1, event.target.value);
+            }}
+          />
         </label>
         <label>
-          <input type="text" name="name" placeholder="Player 2 Name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Player 2 Name"
+            onChange={(event) => {
+              handleNameChange(2, event.target.value);
+            }}
+          />
         </label>
         <div>
           <Slider
@@ -31,7 +71,7 @@ export default function StartPage() {
           <p>slide to select a goal</p>
         </div>
 
-        <button onClick={console.log("hi")}>LET'S START</button>
+        <button type="submit">LET'S START</button>
       </form>
     </div>
   );
